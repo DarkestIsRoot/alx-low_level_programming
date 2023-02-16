7a2f968c4eff1ecfbb862731c02be5f0b0f3439f
DarkestIsRoot <tsheporaselabe101@gmail.com>
2023-02-16 09:33:04 -0800

    My first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
+/**
+ * main - Prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
+ */
 #include <stdio.h>
 
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+    return (0);
 }
-
